909169bb99f43cf5a09ec3529feaccc54fbd0fbe
blondinet <xavier.jacog--guizon@orange.fr>
2020-11-13 09:14:47 +0100

    ajout du pathfinding

--- Lejos_project/src/comportement/Deplacement.java
+++ Lejos_project/src/comportement/Deplacement.java
@@ -1,7 +1,53 @@
 package comportement;
 
+import ressource.*;
+
 public class Deplacement {
 
+	private Position[] listPosCheminGoal;
+	private Robot robot;
+	
+	/**
+	 * @Modification Xavier Jacob--Guizon
+	 * @param r le robot dont on cherche les chemins
+	 */
+	public Deplacement(Robot r) {
+		this.robot = r;
+	}
+	
+	/**
+	 * Fonction qui cherche le chemin et le stock dans listPosCheminGoal
+	 * @Modification Xavier Jacob--Guizon
+	 */
+	public void rechercheChemin() {
+		Position posCourante = this.robot.getCaseCourante();
+		Position goal = this.robot.getCaseGoal();
+		// On recherche le chemin en x (en largeur)
+		while(posCourante.getPosX() > goal.getPosX()) {
+			this.listPosCheminGoal[this.listPosCheminGoal.length+1]=posCourante;
+			posCourante.setPosX(posCourante.getPosX()-1);
+		}
+		while(posCourante.getPosX() < goal.getPosX()) {
+			this.listPosCheminGoal[this.listPosCheminGoal.length+1]=posCourante;
+			posCourante.setPosX(posCourante.getPosX()+1);
+		}
+		
+		// On recherche le chemin en y (en longeur)
+		while(posCourante.getPosY() > goal.getPosY()) {
+			this.listPosCheminGoal[this.listPosCheminGoal.length+1]=posCourante;
+			posCourante.setPosY(posCourante.getPosY()-1);
+		}
+		while(posCourante.getPosY() < goal.getPosY()) {
+			this.listPosCheminGoal[this.listPosCheminGoal.length+1]=posCourante;
+			posCourante.setPosY(posCourante.getPosY()+1);
+		}
+		
+		for(Position val : this.listPosCheminGoal) {
+			System.out.println(val);
+		}
+	}
+	
+	
 	/**
 	 * Fonction qui analyse le tableau et 
 	 * la position du robot ainsi que la position futur du robot 
@@ -10,6 +56,20 @@ public class Deplacement {
 	 */
 	public boolean previsionDeplacement() {
 		return false;
+		//Le robot à un argument de case courante
+		//Le robot à un argument de case futur
+		//Le robot à un argument de case goal
+		//Le robot à la taille d'une case et d'une ligne
+		
+		
+		
+	}
+	
+	/**
+	 * methode qui prend en parametre une liste et fait ce déplacer le robot par rapport aux position de la liste
+	 * @Modification Xavier Jacob--Guizon
+	 */
+	public void deplacementList() {
 		
 	}
 	
---
+++ bin/README.md
@@ -0,0 +1,2 @@
+# M1_robot_alternant
+Le projet de robotique pour le groupe d'alternants
